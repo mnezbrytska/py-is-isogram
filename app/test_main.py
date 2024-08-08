a9dafa7d511f362df7cfa9595916824ab73bb2f2
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-08 22:32:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_with_different_letters() -> None:
+    assert is_isogram("flower") is True
+
+
+def test_uppercase_different_letters() -> None:
+    assert is_isogram("OSTAP") is True
+
+
+def test_with_the_same_letter() -> None:
+    assert is_isogram("apple") is False
+
+
+def test_with_uppercase_not_different() -> None:
+    assert is_isogram("Alphabet") is False
+
+
+def test_upper_lower_different() -> None:
+    assert is_isogram("Custom") is True
